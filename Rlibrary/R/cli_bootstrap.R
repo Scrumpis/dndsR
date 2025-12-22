7f0de3852b6eca833aef27eb2ed2ad075bc07f75
Nick Johnson <nick93johnson@gmail.com>
2025-12-21 19:38:29 -0500

    Add cli_bootstrap_path function for environment setup

---
+++ Rlibrary/R/cli_bootstrap.R
@@ -0,0 +1,13 @@
+cli_bootstrap_path <- function() {
+  dndsr_auto <- tolower(Sys.getenv("AUTO_SHIMS", "1"))
+  run_prefix <- Sys.getenv("RUN_PREFIX", "")
+  shims_dir  <- Sys.getenv("SHIMS_DIR", path.expand("~/.dndsr/shims"))
+
+  if (dndsr_auto %in% c("1","true","yes") &&
+      nzchar(run_prefix) &&
+      dir.exists(shims_dir)) {
+    Sys.setenv(PATH = paste(shims_dir, Sys.getenv("PATH"), sep = .Platform$path.sep))
+  }
+
+  invisible(TRUE)
+}
