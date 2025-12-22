f45b6a9a562e351d82dfa8f3f2b19fb0e1ff82ae
Nick Johnson <nick93johnson@gmail.com>
2025-12-21 22:41:26 -0500

    Refactor cli_go_enrichment function structure

--- Rlibrary/R/cli_go_enrichment.R
+++ Rlibrary/R/cli_go_enrichment.R
@@ -1,4 +1,5 @@
 #' @keywords internal
-cli_go_enrichment <- function(...) {
-  do.call(go_enrichment, list(...))
-}
+cli_go_enrichment <- structure(
+  function(...) do.call(go_enrichment, list(...)),
+  target = "go_enrichment"
+)
